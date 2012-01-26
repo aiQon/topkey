6c0b24682775d4caa9db78e5bb34c8c51061aedb
Stanislaus Stelle <stas.stelle@gmail.com>
2012-01-26 15:28:47 +0100

    changed delay between node messages to 100 ms (need to figure out how long I can get before the VM does not let me, bc of the context sitch taking longer)

--- src/de/cased/serial/SerialWorker.java
+++ src/de/cased/serial/SerialWorker.java
@@ -59,7 +59,7 @@ public class SerialWorker extends Thread {
 					
 					try {
 						logger.log(Level.INFO, "Current Thread falling asleep: " + Thread.currentThread().getName());
-						this.wait(5000); //can be lowered to 8, I think...
+						this.wait(100); //can be lowered to 8, I think...
 					} catch (Exception e) {
 						logger.log(Level.SEVERE, "SerialWorker failed falling asleep.");
 						System.out.println("Message:" + e.getMessage());
